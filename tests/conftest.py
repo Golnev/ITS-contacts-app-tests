f346e6c9595fb31ff3686f1ebe725c8e570e53c5
golnev <golnev@itspartner.net>
2025-03-27 10:02:44 +0100

    fix browser fixture (firefox --headless)

--- tests/conftest.py
+++ tests/conftest.py
@@ -116,7 +116,7 @@ def browser(pytestconfig):
         service = None
 
         if docker_args:
-            options.add_argument("--headless=new")
+            options.add_argument("--headless")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--disable-gpu")
